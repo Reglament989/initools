567fd7987fd5e74f7d253dc37f3bc8ef732a778d
Reglament989 <officialreglament989@protonmail.ch>
2020-04-09 23:32:48 +0300

    final for now

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
 import setuptools
-from py_switch import __version__
-
-def check_version(way=False):
-    if way:
-        with open('.config', 'r') as f:
-            return f.read()
-    else:
-        with open('.config', 'w') as f:
-            f.write(py_switch.__version__)
-
-
-if check_version(True) == __version__:
-    raise Exception("Edit version, for deployment")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="py-switch",
-    version=__version__,
+    version='0.0.5',
     author="Reglament989",
     author_email="officialreglament989@protonmail.ch",
     description="Small tools for other languages",
