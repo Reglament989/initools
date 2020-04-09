80294726f7fcbad1063afc21f127816162880a70
Reglament989 <officialreglament989@protonmail.ch>
2020-04-09 17:55:57 +0300

    Init

---
+++ test.py
@@ -0,0 +1,78 @@
+from switch import IniSwitch
+import random, logging, unittest
+
+logging.basicConfig(
+    format="%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] %(message)s",
+    level=logging.DEBUG,
+)
+
+def test_basic():
+    item = IniSwitch(random.randrange(0,20))
+    while True:
+        if item.case(random.randrange(0,20)):
+            return
+        else:
+            pass
+
+    logging.info(f"Tested item [basic]: {item}")
+
+def test_attempts():
+    item = IniSwitch(random.randrange(0,20), attempts=random.randrange(0,15))
+    while True:
+        try:
+            if item.case(random.randrange(0,20)):
+                return
+            else:
+                pass
+        except StopIteration:
+            return
+        except Exception as e:
+            raise e
+
+    logging.info(f"Tested item [attempts]: {item}")
+
+def test_on_success():
+    item = IniSwitch(random.randrange(0,20), on_success=function_on_success)
+    while True:
+        if item.case(random.randrange(0,20)):
+            return
+        else:
+            pass
+
+    logging.info(f"Tested item [on_success]: {item}")
+
+def test_on_fail():
+    item = IniSwitch(random.randrange(0,20), on_fail=function_on_fail)
+    while True:
+        if item.case(random.randrange(0,20)):
+            return
+        else:
+            pass
+
+    logging.info(f"Tested item [on_fail]: {item}")
+
+def test_together():
+    item = IniSwitch(random.randrange(0,20),
+        on_success=function_on_success,
+        on_fail=function_on_fail,
+        attempts=random.randrange(0,15))
+    while True:
+        try:
+            if item.case(random.randrange(0,20)):
+                return
+            else:
+                pass
+        except StopIteration:
+            return
+        except Exception as e:
+            raise e
+
+    logging.info(f"Tested item [together]: {item}")
+
+def function_on_fail():
+    i = random.random()
+    logging.info(f"Function on_fail complite, {i}")
+
+def function_on_success():
+    i = random.random()
+    logging.info(f"Function on_success complite, {i}")
